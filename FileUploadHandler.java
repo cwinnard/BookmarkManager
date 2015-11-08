48221dc5fabc8de089f92a609fabdb401e10af0f
jtsjeremy <jts@loopctrl.com>
2015-11-08 14:14:38 -0500

    FileUploadHandler initial commit

---
+++ FileUploadHandler.java
@@ -0,0 +1,69 @@
+package com.refRes.upload;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+/**
+* Servlet to handle File upload request from Client
+* @author Javin Paul
+*/
+public class FileUploadHandler extends HttpServlet {
+   /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+private final String UPLOAD_DIRECTORY = "C:/uploads";
+ 
+   @Override
+   protected void doPost(HttpServletRequest request, HttpServletResponse response)
+           throws ServletException, IOException {
+     
+       //process only if its multipart content
+       if(ServletFileUpload.isMultipartContent(request)){
+           try {
+               List<FileItem> multiparts = new ServletFileUpload(
+                                        new DiskFileItemFactory()).parseRequest(request);
+             
+               for(FileItem item : multiparts){
+                   if(!item.isFormField()){
+                       String name = new File(item.getName()).getName();
+                       item.write( new File(UPLOAD_DIRECTORY + File.separator + name));
+                   }
+               }
+          
+              //File uploaded successfully
+              request.setAttribute("message", "File Uploaded Successfully");
+           } catch (Exception ex) {
+              request.setAttribute("message", "File Upload Failed due to " + ex);
+           }          
+        
+       }else{
+           request.setAttribute("message",
+                                "Sorry this Servlet only handles file upload request");
+       }
+      
+       Parser parser = new Parser();
+		List<Bookmark> bookmarkList = parser.populateBookmarkList();
+		for (Bookmark bookmark : bookmarkList) {
+			System.out.println(bookmark.getName());
+			System.out.println(bookmark.getUrl());
+		}
+		BookmarkDB db = new BookmarkDB();
+		db.saveBookmarksToDB(bookmarkList);
+		
+		request.setAttribute("bookmarkList", bookmarkList);
+       
+   
+       request.getRequestDispatcher("/result.jsp").forward(request, response);
+    
+   }
+ 
+}
